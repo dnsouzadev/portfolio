a41f9ef8ab43293664076ec7def5692cd0a239ca
Daniel Souza <workdndsza@gmail.com>
2024-07-04 15:07:42 -0300

    iniciando o projeto

---
+++ src/components/Footer.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillInstagram
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/dnsouzadev"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/dnsouzadev/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/dnsouzadev"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillInstagram
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import Tilt from "react-parallax-tilt";
+import myImg from "../../Assets/avatar.svg";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              SAIBA MAIS <span className="purple"> SOBRE </span> MIM
+            </h1>
+            <p className="home-about-body">
+              Sempre fui apaixonado por tecnologia e computadores, entao… 🤷‍♂️
+              <br />
+              <br />Tentando ser fluente em
+              <i>
+                <b className="purple"> Java e Python. </b>
+              </i>
+              <br />
+              <br />
+              <i>
+              Meu interesse esta em  &nbsp;
+
+                <b className="purple">desenvolvimento backend </b> e no momento estudando {" "}
+                <b className="purple">
+                React
+                </b>
+              </i>
+              {" "} para o frontend.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>CONTATE ME</h1>
+            <p>
+              Sinta-se livre para <span className="purple">conversar </span>comigo.
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/dnsouzadev"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/dnsouzadev/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/dnsouzadev"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
